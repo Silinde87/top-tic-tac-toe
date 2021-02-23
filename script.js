36e82374fbf14ba623c1133f0248bc0d8d400e80
Silinde87 <paurodriguezmolina@gmail.com>
2021-02-23 10:03:32 +0100

    feat: Implement swap player functionality

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+"use strict";
+
+let playerOne;
+let playerTwo;
+let currentPlayer;
+let winner;
+
 // -- DOM Elements --
 let gameBoardElement = document.getElementById("gameboard");
 let resetButton = document.getElementById("reset-btn");
+let playerOneLabel = document.getElementById("player-one");
+let playerTwoLabel = document.getElementById("player-two");
 
 // -- Gameboard Object (module) --
 const Gameboard = (function () {
@@ -12,8 +21,8 @@ const Gameboard = (function () {
         let index = 0;
         gameBoard.forEach((slotValue) => {
             let slotElement = document.createElement("div");
-            slotElement.setAttribute("id", "slot-" + index++);
             slotElement.classList.add("game-slot");
+            slotElement.dataset.id = index++;
             slotElement.innerText = slotValue;
             gameBoardElement.appendChild(slotElement);
         });
@@ -30,44 +39,76 @@ const Gameboard = (function () {
     const addMarkToGameBoard = (playerMark, elemIndex) => {
         if (gameBoard[elemIndex] === "") {
             gameBoard[elemIndex] = playerMark;
+            Game.swapCurrentPlayer();
+            renderGameBoard();
         }
-        renderGameBoard();
     };
 
-    return { resetsGameBoard, addMarkToGameBoard };
+    //Check if there are three in a row or gameBoard is full.
+    const isGameOver = () => {
+        if (!gameBoard.includes("")) {
+            winner = "tie";
+            return true;
+        }
+        return false;
+    };
+
+    return { resetsGameBoard, addMarkToGameBoard, isGameOver };
 })();
 
 /* -- Game Object (module) --
     todo:
     - Game is over?
-    - display winner
-    - select player */
+    - display winner*/
 const Game = (function () {
     //Invoke reset gameBoard to start/restart game.
     const startGame = () => {
         Gameboard.resetsGameBoard();
+        playerOne = Player("X");
+        playerTwo = Player("O");
+        currentPlayer = playerOne;
     };
 
-    return { startGame };
+    //Swap between players turn when invoked.
+    const swapCurrentPlayer = () => {
+        if (currentPlayer == playerOne) {
+            currentPlayer = playerTwo;
+            playerTwoLabel.classList.add("current-player");
+            playerOneLabel.classList.remove("current-player");
+        } else {
+            currentPlayer = playerOne;
+            playerOneLabel.classList.add("current-player");
+            playerTwoLabel.classList.remove("current-player");
+        }
+    };
+
+    return { startGame, swapCurrentPlayer };
 })();
 
 // -- Player Object (factory) --
 const Player = (mark) => {
-    let playerMark = mark;
+    this.mark = mark;
 
     //Invoke addMark function and passes the id of clicked elem
     //and playerMark as parameter.
     const addMark = (e) => {
-        let index = e.target.id.split("-")[1];
-        Gameboard.addMarkToGameBoard(playerMark, index);
+        e.stopImmediatePropagation();
+        if (e.target.id === "gameboard") return;
+        let index = e.target.dataset.id;
+        Gameboard.addMarkToGameBoard(currentPlayer.mark, index);
+
+        if (Gameboard.isGameOver()) {
+            gameBoardElement.removeEventListener("click", addMark);
+            console.log("winner: " + winner);
+        }
     };
 
     gameBoardElement.addEventListener("click", addMark);
+    return { mark };
 };
 
 //Events
 resetButton.addEventListener("click", Game.startGame);
 
 //Temporary Initialization
-Gameboard.resetsGameBoard();
-let playerOne = Player("X");
+Game.startGame();
