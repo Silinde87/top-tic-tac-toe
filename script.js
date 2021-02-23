a1ab1e7290ef3f384cae93df206ebf4f3deefc1e
Silinde87 <paurodriguezmolina@gmail.com>
2021-02-23 17:21:43 +0100

    Implement AI with random movement

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ let resetButton = document.getElementById("reset-btn");
 let playerOneLabel = document.getElementById("player-one");
 let playerTwoLabel = document.getElementById("player-two");
 
-// -- Gameboard Object (module) --
+// -- GAMEBOARD OBJECT (module) --
 const Gameboard = (function () {
-    let gameBoard = ["X", "O", "X", "O", "X", "X", "X", "O", "O"];
+    let gameBoard = ["", "", "", "", "", "", "", "", ""];
 
     //Renders the current gameBoard value to the DOM
     const renderGameBoard = () => {
@@ -47,37 +47,37 @@ const Gameboard = (function () {
     //Check if there are three-in-a-row or gameBoard is full.
     const isGameOver = () => {
         //Row checks
-        if(gameBoard[0] == gameBoard[1] && gameBoard[0] == gameBoard[2] && gameBoard[0] !== ""){
+        if (gameBoard[0] == gameBoard[1] && gameBoard[0] == gameBoard[2] && gameBoard[0] !== "") {
             winner = gameBoard[0];
             return true;
         }
-        if(gameBoard[3] == gameBoard[4] && gameBoard[3] == gameBoard[5] && gameBoard[3] !== ""){
+        if (gameBoard[3] == gameBoard[4] && gameBoard[3] == gameBoard[5] && gameBoard[3] !== "") {
             winner = gameBoard[3];
             return true;
         }
-        if(gameBoard[6] == gameBoard[7] && gameBoard[6] == gameBoard[8] && gameBoard[6] !== ""){
+        if (gameBoard[6] == gameBoard[7] && gameBoard[6] == gameBoard[8] && gameBoard[6] !== "") {
             winner = gameBoard[6];
             return true;
         }
         //Columns checks
-        if(gameBoard[0] == gameBoard[3] && gameBoard[0] == gameBoard[6] && gameBoard[0] !== ""){
+        if (gameBoard[0] == gameBoard[3] && gameBoard[0] == gameBoard[6] && gameBoard[0] !== "") {
             winner = gameBoard[0];
             return true;
         }
-        if(gameBoard[1] == gameBoard[4] && gameBoard[1] == gameBoard[7] && gameBoard[1] !== ""){
+        if (gameBoard[1] == gameBoard[4] && gameBoard[1] == gameBoard[7] && gameBoard[1] !== "") {
             winner = gameBoard[1];
             return true;
         }
-        if(gameBoard[2] == gameBoard[5] && gameBoard[2] == gameBoard[8] && gameBoard[2] !== ""){
+        if (gameBoard[2] == gameBoard[5] && gameBoard[2] == gameBoard[8] && gameBoard[2] !== "") {
             winner = gameBoard[2];
             return true;
         }
         //Diagonals checks
-        if(gameBoard[0] == gameBoard[4] && gameBoard[0] == gameBoard[8] && gameBoard[0] !== ""){
+        if (gameBoard[0] == gameBoard[4] && gameBoard[0] == gameBoard[8] && gameBoard[0] !== "") {
             winner = gameBoard[0];
             return true;
         }
-        if(gameBoard[2] == gameBoard[4] && gameBoard[2] == gameBoard[6] && gameBoard[2] !== ""){
+        if (gameBoard[2] == gameBoard[4] && gameBoard[2] == gameBoard[6] && gameBoard[2] !== "") {
             winner = gameBoard[2];
             return true;
         }
@@ -92,19 +92,17 @@ const Gameboard = (function () {
     return { resetsGameBoard, addMarkToGameBoard, isGameOver };
 })();
 
-/* -- Game Object (module) --
-    todo:
-    - display winner*/
+// -- GAME OBJECT (module) --
 const Game = (function () {
     //Invoke reset gameBoard to start/restart game.
     const startGame = () => {
         Gameboard.resetsGameBoard();
-        playerOne = Player("X");
-        playerTwo = Player("O");
+        playerOne = Player("X", "human");
+        playerTwo = Player("O", "AI");        
         currentPlayer = playerOne;
         playerOneLabel.classList.add("current-player");
         playerTwoLabel.classList.remove("current-player");
-        gameBoardElement.addEventListener("click", currentPlayer.addMark)
+        gameBoardElement.addEventListener("click", currentPlayer.addMark);
     };
 
     //Swap between players turn when invoked.
@@ -123,24 +121,49 @@ const Game = (function () {
     return { startGame, swapCurrentPlayer };
 })();
 
-// -- Player Object (factory) --
-const Player = (mark) => {
-    this.mark = mark;
+// -- PLAYER OBJECT (factory) --
+const Player = (markPlayer, typePlayer) => {
+    let mark = markPlayer;
+    let type = typePlayer;
+
+    //Getters
+    const getMark = () => mark;
+    const getType = () => type;    
 
     //Invoke addMark function and passes the id of clicked elem
     //and playerMark as parameter.
     const addMark = (e) => {
-        e.stopImmediatePropagation();
+        //e.stopImmediatePropagation();        
+        //Exit if user doesn't click slot
         if (e.target.id === "gameboard") return;
-        let index = e.target.dataset.id;
-        Gameboard.addMarkToGameBoard(currentPlayer.mark, index);
+        //Exit if the user clicks on the AI's turn
+        if (currentPlayer.getType() == "AI") return;
 
+        //Adds the mark to the gameboard in selected slot.
+        let index = e.target.dataset.id;
+        Gameboard.addMarkToGameBoard(currentPlayer.getMark(), index);
+
+        //AI's turn
+        if (currentPlayer.getType() == "AI"){
+            if (!Gameboard.isGameOver()) addRandomMark();
+        }
+        
+        //Game is over exit.
         if (Gameboard.isGameOver()) {
             gameBoardElement.removeEventListener("click", addMark);
             console.log("winner: " + winner);
         }
     };
-    return { mark, addMark};
+
+    //Selects a random empty slot and adds the mark to the gameboard with a 500ms delay
+    const addRandomMark = () => {
+        let indexRandom;
+        do {
+            indexRandom = Math.floor(Math.random() * 9);
+        } while(gameBoardElement.children.item(indexRandom).innerText !== "");
+        setTimeout(Gameboard.addMarkToGameBoard, 500, currentPlayer.getMark(), indexRandom);            
+    }
+    return { addMark, getMark, getType };
 };
 
 //Temporary Initialization
